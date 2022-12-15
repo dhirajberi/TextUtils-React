a045936619aae001d44e3e41bdcc9076d88279a2
dhirajberi <dhiraj.b@yudiz.com>
2022-12-15 13:54:17 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {userState} from 'react'
+import React from 'react'
 import About from './components/About'
 import Footer from './components/Footer'
 import Header from './components/Header'
@@ -7,7 +7,6 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
 
 const App = () => {
@@ -18,8 +17,8 @@ const App = () => {
     <BrowserRouter>
       <Header Name="Dhiraj"/>
       <Routes> 
-        <Route path="/" element={<TextForm />} />
-        <Route path="about" element={<About />} />
+        <Route path="/TextUtils-React" element={<TextForm />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer Year="2022"/>
     </BrowserRouter>
